1126e789c0d9ddc6a7181b2cda3017913867d88a
Richard Cook <rcook@rcook.org>
2018-07-19 21:25:27 -0700

    Remove indicators

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ $(() => {
     "hp-uQZ-MujA"
   ];
 
-  const liContainer = $("#carousel .carousel-indicators");
   const divContainer = $("#carousel .carousel-inner");
-  const liTemplate = $("#video-li");
-  const divTemplate = $("#video-div");
+  const divTemplate = $("#carousel-template");
 
   for (let i = 0; i < videoIds.length; ++i) {
-    const li = $(liTemplate.html());
-    li.attr("data-slide-to", i.toString());
-
     const div = $(divTemplate.html());
-    div.find(">:first-child").attr("src", `https://www.youtube.com/embed/${videoIds[i]}`);
+    div.find("> :first-child").attr("src", `https://www.youtube.com/embed/${videoIds[i]}`);
 
     if (i == 0) {
-      li.addClass("active");
       div.addClass("active");
     }
 
-    liContainer.append(li);
     divContainer.append(div);
   }
 });
