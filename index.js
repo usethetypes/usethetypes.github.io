e99cfaf6348346e4adcdaa58281c8b4d9e31a281
Richard Cook <rcook@rcook.org>
2018-07-22 11:54:34 -0700

    Use images instead of videos in carousel

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 $(() => {
+  function makeVideoUrl(videoId) {
+    return `https://youtu.be/${videoId}`;
+  }
+
+  function makeImageUrl(videoId) {
+    return `https://img.youtube.com/vi/${videoId}/0.jpg`;
+  }
+
   const videoIds = [
     "iNeLpmjowwQ",
     "cDYn_la-9vg",
     "hp-uQZ-MujA",
     "uB7ES5-JhQA",
     "vcE9BlW74tg",
-    "sP6Iuwd_Nik"
+    "sP6Iuwd_Nik",
+    "9qiRk5la724"
   ];
 
   const divContainer = $("#carousel .carousel-inner");
-  const divTemplate = $("#carousel-template");
+  const divTemplate = $("#carousel-template-image");
 
   for (let i = 0; i < videoIds.length; ++i) {
     const div = $(divTemplate.html());
-    div.find("> :first-child").attr("src", `https://www.youtube.com/embed/${videoIds[i]}?rel=0`);
+
+    const anchor = div.find("> :first-child");
+    anchor.attr("href", makeVideoUrl(videoIds[i]));
+
+    const image = anchor.find("> :first-child");
+    image.attr("src", makeImageUrl(videoIds[i]));
 
     if (i == 0) {
       div.addClass("active");
