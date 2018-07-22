a70082213fc9b9677065c21641a9c56294675861
Richard Cook <rcook@rcook.org>
2018-07-22 16:53:13 -0700

    Get title from noembed.com

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ $(() => {
     return `https://img.youtube.com/vi/${videoId}/0.jpg`;
   }
 
+  function makeNoEmbedUrl(videoId) {
+    return `https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoId}`;
+  }
+
   const videoIds = [
     "iNeLpmjowwQ",
     "cDYn_la-9vg",
@@ -21,18 +25,34 @@ $(() => {
   const divTemplate = $("#carousel-template-image");
 
   for (let i = 0; i < videoIds.length; ++i) {
+    const videoId = videoIds[i];
+
     const div = $(divTemplate.html());
+    div.attr("id", `video-${videoId}`);
 
     const anchor = div.find("> :first-child");
-    anchor.attr("href", makeVideoUrl(videoIds[i]));
+    anchor.attr("href", makeVideoUrl(videoId));
 
     const image = anchor.find("> :first-child");
-    image.attr("src", makeImageUrl(videoIds[i]));
+    image.attr("src", makeImageUrl(videoId));
 
     if (i == 0) {
       div.addClass("active");
     }
 
     divContainer.append(div);
+
+    $.get({
+      url: makeNoEmbedUrl(videoId),
+      dataType: "json"
+    }).then(response => {
+      const slug = `testvideo#${i}`;
+      const anchorTitle = `${slug}: ${response.title}`;
+      anchor
+        .attr("title", anchorTitle)
+        .attr("alt", anchorTitle);
+      div.find("h5").text(response.title);
+      div.find("p").text(slug);
+    });
   }
 });
